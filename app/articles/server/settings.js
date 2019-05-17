b9a3fa658794d2c6467e20e7b95cd6eae910b30e
shubhsherl <shubham.singh094@yahoo.com>
2019-05-17 18:52:58 +0530

    add redirect to articles admin

--- app/articles/server/settings.js
+++ app/articles/server/settings.js
@@ -8,9 +8,14 @@ const defaults = {
 
 Meteor.startup(() => {
 	settings.addGroup('Articles', function() {
-		this.add('Articles_enable', defaults.enable, {
+		this.add('Articles_enabled', defaults.enable, {
 			type: 'boolean',
 			i18nLabel: 'Enable',
+		});
+
+		this.add('Articles_admin_panel', 'Articles_admin_panel', {
+			type: 'action',
+			actionText: 'Article_Admin_Panel',
 		});
 	});
 });
