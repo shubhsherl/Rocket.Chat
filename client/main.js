3bff9f0dc99ceed00c133dfe22bde94bf73c02e1
Karan Bedi <karanbedi0304@gmail.com>
2019-09-20 22:44:27 +0530

    Add sw.js and cache all pages

--- client/main.js
+++ client/main.js
@@ -32,3 +32,18 @@ import './startup/startup';
 import './startup/unread';
 import './startup/userSetUtcOffset';
 import './startup/usersObserve';
+
+if ('serviceWorker' in navigator) {
+	if (navigator.serviceWorker.controller) {
+		console.log('Active service worker found, no need to register');
+	} else {
+		// Register the service worker
+		navigator.serviceWorker
+			.register('sw.js', {
+				scope: './',
+			})
+			.then(function(reg) {
+				console.log(`Service worker has been registered for scope: ${ reg.scope }`);
+			});
+	}
+}
