c5eb73e5587400509f2ab7d2a6419a15aa6cee76
shubhsherl <shubham.singh094@yahoo.com>
2019-12-12 19:55:21 +0530

    fix searchBar for mobile client

--- app/ui-message/client/popup/messagePopup.js
+++ app/ui-message/client/popup/messagePopup.js
@@ -237,7 +237,7 @@ Template.messagePopup.onRendered(function() {
 	if (this.data.getInput != null) {
 		this.input = typeof this.data.getInput === 'function' && this.data.getInput();
 	} else if (this.data.input) {
-		this.input = this.parentTemplate().find(this.data.input);
+		this.input = this.parentTemplate(this.data.parent).find(this.data.input);
 	}
 	if (this.input == null) {
 		console.error('Input not found for popup');
--- app/ui-sidenav/client/sidebarHeader.js
+++ app/ui-sidenav/client/sidebarHeader.js
@@ -47,12 +47,24 @@ const extendedViewOption = (user) => {
 };
 
 const showToolbar = new ReactiveVar(false);
+let hideHeader = true;
 
 export const toolbarSearch = {
 	shortcut: false,
-	show(fromShortcut) {
+	clear() {
+		const $inputMessage = $('.js-input-message');
+
+		if ($inputMessage.length === 0) {
+			return;
+		}
+
+		$inputMessage.focus();
+		$(selectorSearch).val('');
+	},
+	show(fromShortcut, header = true) {
 		menu.open();
 		showToolbar.set(true);
+		hideHeader = header;
 		this.shortcut = fromShortcut;
 	},
 	close() {
@@ -74,6 +86,7 @@ const toolbarButtons = (/* user */) => [{
 {
 	name: t('Search'),
 	icon: 'magnifier',
+	condition: () => !isMobile(),
 	action: () => {
 		toolbarSearch.show(false);
 	},
@@ -83,7 +96,7 @@ const toolbarButtons = (/* user */) => [{
 	icon: '',
 	condition: () => isMobile(),
 	action: () => {
-		toolbarSearch.show(false);
+		toolbarSearch.show(false, false);
 	},
 	searchBar: true,
 },
@@ -351,6 +364,9 @@ Template.sidebarHeader.helpers({
 	showToolbar() {
 		return showToolbar.get();
 	},
+	hideHeader() {
+		return hideHeader;
+	},
 });
 
 Template.sidebarHeader.events({
@@ -361,10 +377,13 @@ Template.sidebarHeader.events({
 		return this.action && this.action.apply(this, [e]);
 	},
 	'click #searchBar'() {
-		toolbarSearch.show(false);
+		toolbarSearch.show(false, false);
 	},
 	'focus #searchBar'() {
-		toolbarSearch.show(false);
+		toolbarSearch.show(false, false);
+	},
+	'blur #searchBar'() {
+		toolbarSearch.clear();
 	},
 	'click .sidebar__header .avatar'(e) {
 		if (!(Meteor.userId() == null && settings.get('Accounts_AllowAnonymousRead'))) {
