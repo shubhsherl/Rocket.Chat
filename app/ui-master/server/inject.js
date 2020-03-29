8f124c8d937d55cb929762fc0ab6f48ffc7c8482
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2020-03-29 08:19:01 +0000

    Remove console log

--- app/ui-master/server/inject.js
+++ app/ui-master/server/inject.js
@@ -72,7 +72,6 @@ Meteor.startup(() => {
 	});
 
 	settings.get('theme-color-rc-color-primary', (key, value) => {
-		console.log({ key, value });
 		const escapedValue = s.escapeHTML(value);
 		injectIntoHead(key, `<meta name="msapplication-TileColor" content="${ escapedValue }" />`
 							+ `<meta name="theme-color" content="${ escapedValue }" />`);
